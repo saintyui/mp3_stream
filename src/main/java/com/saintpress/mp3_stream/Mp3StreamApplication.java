cda8cf5f5d60d8a803472f26c24ea80ba02b9238
peter <saintyui@gmail.com>
2022-08-04 01:20:56 +0900

    첫 시작

---
+++ src/main/java/com/saintpress/mp3_stream/Mp3StreamApplication.java
@@ -0,0 +1,13 @@
+package com.saintpress.mp3_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mp3StreamApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/saintpress/mp3_stream/test/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.saintpress.mp3_stream.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public List<String> Hello(){
+        return Arrays.asList("안녕하세요", "Hello");
+    }
+}
