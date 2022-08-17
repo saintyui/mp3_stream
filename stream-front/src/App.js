08e6428a2cde0db2217d8aa5f452623bb8e49678
peter <saintyui@gmail.com>
2022-08-17 23:11:33 +0900

    취약점 처리 및 h5-audio-player 적용

--- stream-front/src/App.js
+++ stream-front/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import Datatable from "./component/Datatable";
+import AudioPlayer from "react-h5-audio-player";
+import 'react-h5-audio-player/lib/styles.css';
 
 
 function App() {
@@ -25,10 +27,7 @@ function App() {
               MP3 스트리밍 테스트입니다.
           </h2>
           <Datatable></Datatable>
-
-        <ul>
-          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-        </ul>
+          <AudioPlayer />
       </header>
     </div>
   );
